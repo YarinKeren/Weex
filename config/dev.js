4111a185689d1896884ea14335e1f9c7945c01cf
Ido Halbany <idohalbany97@gmail.com>
2023-11-07 14:12:21 +0200

    changed to local production

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 export default {
-  // dbURL: 'mongodb://127.0.0.1:27017',
-  dbURL: 'mongodb+srv://yarin:yarin551@weex.0slqmf9.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
+  dbURL: 'mongodb://127.0.0.1:27017',
+  // dbURL: 'mongodb+srv://yarin:yarin551@weex.0slqmf9.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
   dbName: 'WAP_DB',
 }
